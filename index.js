bb88331f9260b08535e253a54463df55374e001c
Gustavo <gugofogomes@gmail.com>
2026-02-10 22:33:31 -0300

    Deixei um pouco mais bonito o codigo e adicionei selecionar

--- index.js
+++ index.js
@@ -1,24 +1,54 @@
-const quadrados = document.getElementById("divQuadradosDia");
+const quadradosDiv = document.getElementById("divQuadradosDia");
+const matriz = Array.from({length: 60}, () => Array(5).fill(false));
 
-const diasSemana = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"];
-const nomesMeses = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"];
+criarCalendario();
 
-const hoje = new Date();
-let htmlConteudo = "";
+const dias = document.querySelectorAll(".dia");
+const meses = document.querySelectorAll(".mes");
+const diaDaSemanas = document.querySelectorAll(".diaDaSemana");
+const quadradosDias = document.querySelectorAll(".quadradoDia");
 
-for(let i = 0; i < 60; i++){
-    let dataFutura = new Date();
-    dataFutura.setDate(hoje.getDate() + i);
+quadradosDias.forEach(quadrado => {
 
-    let diaNumero = dataFutura.getDate();
-    let mesNome = nomesMeses[dataFutura.getMonth()];
-    let diaSemanaNome = diasSemana[dataFutura.getDay()];
+    quadrado.addEventListener("click", evento => {
+        quadradosDias.forEach(quadrado => quadrado.style.backgroundColor = "white");
 
-    htmlConteudo += `
-        <div class="quadradoDia" id="dia${i}">
-            <p class="diaDaSemana">${diaSemanaNome}</p>
-            <p class="dia">${diaNumero}</p>
-            <p class="mes">${mesNome}</p>
-        </div>`;
+        let quadrado = evento.target;
+        if(quadrado.classList != "quadradoDia") quadrado = quadrado.parentElement;
+        
+        quadrado.style.backgroundColor = "rgb(245, 245, 245)";
+        console.log(quadrado);
+    })
+})
+
+function checar(index){
+    const tarefa = document.getElementById(`tarefa${index}`);
+    const divPai = tarefa.parentElement;
+    
+    tarefa.checked ? divPai.setAttribute("id", "tarefaFeita") : divPai.setAttribute("id", "tarefaNaoFeita") 
 }
-quadrados.innerHTML = htmlConteudo;
+
+function criarCalendario() {
+    const diasSemana = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"];
+    const nomesMeses = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"];
+    
+    const hoje = new Date();
+    let htmlConteudo = "";
+    
+    for(let i = 0; i < 60; i++){
+        let dataFutura = new Date();
+        dataFutura.setDate(hoje.getDate() + i);
+    
+        let diaNumero = dataFutura.getDate();
+        let mesNome = nomesMeses[dataFutura.getMonth()];
+        let diaSemanaNome = diasSemana[dataFutura.getDay()];
+    
+        htmlConteudo += `
+            <div class="quadradoDia" id="dia${i}">
+                <p class="diaDaSemana">${diaSemanaNome}</p>
+                <p class="dia">${diaNumero}</p>
+                <p class="mes">${mesNome}</p>
+            </div>`;
+    }
+    quadradosDiv.innerHTML = htmlConteudo;
+}
