1336401e3ba62504fddf4315caeb48439a473e4d
Gustavo <gugofogomes@gmail.com>
2026-02-10 21:24:15 -0300

    Consegui fazer os dias

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const quadrados = document.getElementById("divQuadradosDia");
 
-const data = new Date();
-let mes = data.getMonth();
-let ano = data.getFullYear();
+const diasSemana = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sáb"];
+const nomesMeses = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"];
+
+const hoje = new Date();
+let htmlConteudo = "";
 
 for(let i = 0; i < 60; i++){
-    quadrados.innerHTML += `<div class="quadradoDia" id="dia${i}">
-                            <p class="diaDaSemana">segunda</p>
-                            <p class="dia">${i}</p>
-                            <p class="mes">Fev</p>
-                            </div>`
+    let dataFutura = new Date();
+    dataFutura.setDate(hoje.getDate() + i);
+
+    let diaNumero = dataFutura.getDate();
+    let mesNome = nomesMeses[dataFutura.getMonth()];
+    let diaSemanaNome = diasSemana[dataFutura.getDay()];
+
+    htmlConteudo += `
+        <div class="quadradoDia" id="dia${i}">
+            <p class="diaDaSemana">${diaSemanaNome}</p>
+            <p class="dia">${diaNumero}</p>
+            <p class="mes">${mesNome}</p>
+        </div>`;
 }
+quadrados.innerHTML = htmlConteudo;
